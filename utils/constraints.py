a283d290bdbc9a1cf53069c75ee5f522000cd180
Thiago Lizier Rissi <thiago.rissi@usp.br>
2025-07-30 04:03:21 -0300

    First implementation

---
+++ utils/constraints.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn.functional as F
+from utils.other_utils import transform_tensor_to_image
+
+
+def brightness_constraint(x: torch.Tensor, target: float = 0.4, tol: float = 0.05):
+    """
+    Ensures the mean brightness of the image is close to the target value.
+    """
+    x = torch.clamp(x, 0, 1)
+    x = x.view(x.size(0), -1)
+    mean_brightness = x.mean()
+    g_val = F.relu(mean_brightness - target) - tol
+    g_val = g_val.sum()
+
+    return g_val
+
+
+def center_constraint(
+    x: torch.Tensor, max_radius: float = 5.0, center: tuple[int, int] = (10, 10)
+):
+    """
+    Ensures digit center is within max_radius of target center.
+    """
+    cx_target, cy_target = center
+
+    x_pos = torch.relu(x) + 1e-6
+    B, C, H, W = x.shape
+    ys, xs = torch.meshgrid(
+        torch.arange(H, device=x.device),
+        torch.arange(W, device=x.device),
+        indexing="ij",
+    )
+    xs = xs.float().unsqueeze(0).unsqueeze(0)
+    ys = ys.float().unsqueeze(0).unsqueeze(0)
+    mass = x_pos.sum(dim=(2, 3), keepdim=True)
+    cx = (xs * x_pos).sum(dim=(2, 3), keepdim=True) / mass
+    cy = (ys * x_pos).sum(dim=(2, 3), keepdim=True) / mass
+    dist = torch.sqrt((cx - cx_target) ** 2 + (cy - cy_target) ** 2)
+    g_val = dist - max_radius
+    return g_val.sum()
+
+
+def classifier_constraint(
+    x: torch.Tensor,
+    classifier: torch.nn.Module,
+    target_class: int = 3,
+    epsilon: float = 0.2,
+):
+    """
+    Creates a classification constraint function for constrained diffusion sampling.
+    """
+
+    classifier.eval()  # Make sure dropout/batchnorm aren't active
+
+    logits = classifier(x)  # (batch, num_classes)
+    ce_loss = F.cross_entropy(
+        logits,
+        torch.full((x.size(0),), target_class, device=x.device, dtype=torch.long),
+        reduction="none",
+    )
+    g_val = ce_loss - epsilon  # (batch,)
+    return g_val.sum()  # must return scalar for grad
---
+++ utils/other_utils.py
@@ -0,0 +1,103 @@
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from IPython.display import Image
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+
+def save_animation(xs, gif_name, interval=300, repeat_delay=5000):
+    fig = plt.figure()
+    plt.axis("off")
+    imgs = []
+
+    for x_t in xs:
+        im = plt.imshow(x_t, animated=True)
+        imgs.append([im])
+
+    animate = animation.ArtistAnimation(
+        fig, imgs, interval=interval, repeat_delay=repeat_delay
+    )
+    animate.save(gif_name)
+
+
+def load_fashionMNIST(data_transform, train=True):
+    return torchvision.datasets.FashionMNIST(
+        "./data/",
+        download=True,
+        train=train,
+        transform=data_transform,
+    )
+
+
+def load_transformed_fashionMNIST(img_size, batch_size):
+    data_transforms = [
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),  # Scales data into [0,1]
+        transforms.RandomHorizontalFlip(),
+        transforms.Lambda(lambda t: (t * 2) - 1),  # Scale between [-1, 1]
+    ]
+
+    data_transform = transforms.Compose(data_transforms)
+    train_set = load_fashionMNIST(data_transform, train=True)
+    test_set = load_fashionMNIST(data_transform, train=False)
+    data = torch.utils.data.ConcatDataset([train_set, test_set])
+    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
+    return data, dataloader
+
+
+def load_MNIST(data_transform, train=True):
+    return torchvision.datasets.MNIST(
+        "./data/",
+        download=True,
+        train=train,
+        transform=data_transform,
+    )
+
+
+def load_transformed_MNIST(img_size, batch_size):
+    data_transforms = [
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),  # Scales data into [0,1]
+    ]
+
+    data_transform = transforms.Compose(data_transforms)
+    train_set = load_MNIST(data_transform, train=True)
+    test_set = load_MNIST(data_transform, train=False)
+    data = torch.utils.data.ConcatDataset([train_set, test_set])
+    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
+    return data, dataloader
+
+
+def transform_tensor_to_image(tensor: torch.Tensor):
+    """
+    Transforms a tensor into a PIL image.
+    """
+    reverse_transforms = transforms.Compose(
+        [
+            transforms.Lambda(lambda t: (t + 1) / 2),
+            transforms.Lambda(lambda t: torch.minimum(torch.tensor([1]), t)),
+            transforms.Lambda(lambda t: torch.maximum(torch.tensor([0]), t)),
+            transforms.ToPILImage(),
+        ]
+    )
+    return reverse_transforms(tensor)
+
+
+def show_tensor_image(tensor: torch.Tensor):
+    image = transform_tensor_to_image(tensor[0].detach().cpu())
+
+    plt.imshow(image, cmap="gray")
+
+
+def plot_generated_images(noise, result):
+    plt.figure(figsize=(8, 8))
+    nrows = 1
+    ncols = 2
+    samples = {"Noise": noise, "Generated Image": result}
+    for i, (title, img) in enumerate(samples.items()):
+        ax = plt.subplot(nrows, ncols, i + 1)
+        ax.set_title(title)
+        show_tensor_image(img)
+    plt.show()
